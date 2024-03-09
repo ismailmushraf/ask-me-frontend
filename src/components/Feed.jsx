9884c46bf1efbd9247e804d50e1756d8a0d915cd
Ismail Musharaf <ismailmushraf@Ismails-MacBook-Pro.local>
2024-03-09 22:25:11 +0530

    made change in Feed to redirect to sigin if user is not already signed in

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,11 +1,19 @@
 import { Post } from "../modals/Post";
 import { useState, useEffect } from "react";
 import { fetchQuestions } from "../api";
+import { useAuth } from "../AuthContext";
+import { useNavigate } from "react-router-dom";
 
 export function Feed() {
   const [questions, setQuestions] = useState([]);
+  const { isAuthenticated } = useAuth();
+  const navigate = useNavigate();
 
   useEffect(() => {
+    if (!isAuthenticated) {
+      navigate('/signin');
+    }
+
     try {
       const getQuestions = async () => {
         const response = await fetchQuestions();
